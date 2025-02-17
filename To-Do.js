3b6eb6bf56875bbd79954aa448c61bf1493beb19
Mathan-2007 <mathan007.2007@gmail.com>
2025-02-17 14:10:36 +0530

    updated

--- To-Do.js
+++ To-Do.js
@@ -1,14 +1,24 @@
 function abc(){
     var inp = document.getElementById("add-task").value;
+    var date=document.getElementById("date").value;
+    if(inp==''||date==''){
+        alert("please enter new task or date")}
+    else{
     var div2=document.getElementById("ul")
-var out =document.createElement("li");
-out.innerHTML=inp +'<button class="del" onclick="del(event)">Delete</button>' +'<button class="com">Complete</complete>'; 
+var out =document.createElement("tr");
+out.innerHTML='<td class="tablee">'+inp +'</td id="d">'+'<td>'+date+'</td>'+'<td id="td2">'+'<button class="del" onclick="del(event)">Delete</button>'+'</td>'+'<td id="td3">'+'<button  class="com" onclick="com(event)">Complete</button>'+'</td>'; 
 div2.append(out);
-document.getElementById("add-task").value = '';
+document.getElementById("add-task").value = '';}
 }
 function del (event){
-   event.target.parentElement.remove()
+   event.target.parentElement.parentElement.remove()
 }
-function com(){
+function com(event){
+   
+    var task = event.target.parentElement.parentElement;
+    task.style.textDecoration = "line-through"; 
     
+}
+function cl(){
+
 }
